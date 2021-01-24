313c5165d4d5be3b9df51c9ea849992fcf522c68
sjay <saira.jamil64@gmail.com>
2021-01-24 20:29:48 +0100

    first commit

---
+++ Assets/Clickmouse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickmouse : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 100.0f;
+    float xrotation =0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate the entire body of the player around X axis and also look up and down.
+        float forwardX = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y");
+        xrotation -= mouseY;
+        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
+     
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        // making the player move forward and backward
+
+
+
+    }
+}
---
+++ Assets/Playermove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{  private float speed= 20.0f;
+    private float turnSpeed = 25.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardX = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardX);
+        transform.Translate(Vector3.right* Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
